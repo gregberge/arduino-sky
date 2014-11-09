4cbe8383bb96527f57885a8e972d748636de1adf
Greg Bergé <neoziro@gmail.com>
2014-11-09 23:36:48 +0100

    First version.

---
+++ README.md
@@ -0,0 +1,15 @@
+# arduino-sky
+
+Reproduce sky using Arduino, WebSocket and SVG.
+
+## Getting started
+
+```
+npm install
+bower install
+npm start
+```
+
+## License
+
+MIT
---
+++ server.js
@@ -0,0 +1,37 @@
+var five = require('johnny-five');
+var express = require('express');
+var http = require('http');
+var Primus = require('primus');
+
+// Create application.
+var app = express();
+
+// Create server using application.
+var server = http.createServer(app);
+
+// Create a primus instance binded on HTTP server.
+var primus = new Primus(server);
+
+// Expose static stuff.
+app.use(express.static('.'));
+
+// Listen.
+server.listen(process.env.PORT || 3000);
+
+// Create board.
+var board = new five.Board();
+
+// Wait for board to be ready.
+board.on('ready', function() {
+  // Create photoresistor.
+  var photoresistor = new five.Sensor({
+    pin: 'A0',
+    freq: 50,
+    range: [80, 900]
+  });
+
+  // Scale photoresistor on 0-100 and transmit data over primus.
+  photoresistor.scale([0, 100]).on('data', function() {
+    primus.write({value: this.value});
+  });
+});
